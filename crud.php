e1a11e8d4c295c11c824f10a394c6f22ee383e08
VirtualAI3 <elacaguate123>
2023-05-25 20:20:33 -0500

    Begin

--- crud.php
+++ crud.php
@@ -14,4 +14,25 @@ if (isset($_FILES['image'])) {
     $estado=false;
     $data=['error' => $error,'estado' => $estado];
 }
+$file = $_FILES["image"]["name"]; //Nombre de nuestro archivo
+
+$url_temp = $_FILES["image"]["tmp_name"]; //Ruta temporal a donde se carga el archivo 
+
+//dirname(__FILE__) nos otorga la ruta absoluta hasta el archivo en ejecución
+$url_insert = dirname(__FILE__) . "/images"; //Carpeta donde subiremos nuestros archivos
+
+//Ruta donde se guardara el archivo, usamos str_replace para reemplazar los "\" por "/"
+$url_target = str_replace('\\', '/', $url_insert) . '/' . $file;
+
+//Si la carpeta no existe, la creamos
+if (!file_exists($url_insert)) {
+    mkdir($url_insert, 0777, true);
+};
+
+//movemos el archivo de la carpeta temporal a la carpeta objetivo y verificamos si fue exitoso
+if (move_uploaded_file($url_temp, $url_target)) {
+    echo "El archivo ha sido cargado con éxito.";
+} else {
+    echo "Ha habido un error al cargar tu archivo.";
+}
 print json_encode($data, JSON_UNESCAPED_UNICODE);
