7801f18d6c15755bb9b48dfb3371bb17dee3812b
VirtualAI3 <elacaguate123>
2023-05-25 20:08:58 -0500

    Begin

--- crud.php
+++ crud.php
@@ -8,10 +8,10 @@ $estado=false;
 if (isset($_FILES['image'])) {
     $error="Se cargo el archivo";
     $estado=true;
-    $data=['error' => $error];
+    $data=['error' => $error,'estado' => $estado];
 } else {
     $error="El archivo no se ha cargado";
     $estado=false;
-    $data=['error' => $error];
+    $data=['error' => $error,'estado' => $estado];
 }
 print json_encode($data, JSON_UNESCAPED_UNICODE);
